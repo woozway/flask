e16531c3493216bae0232deeeba0fad4df51f841
woozway <22060468@zju.edu.cn>
2026-01-18 12:34:36 +0800

    -

---
+++ Introduction to SQL and Databases for Back-End Web Apps/Databases in Flask/FlaskFM/models.py
@@ -0,0 +1,18 @@
+from app import app, db
+
+#the User model: each user has a username, and a playlist_id foreign key referring
+#to the user's Playlist
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key = True)
+  username = db.Column(db.String(50), index = True, unique = True) 
+  playlist_id = db.Column(db.Integer,  db.ForeignKey('playlist.id'))
+  
+  #representation method
+  def __repr__(self):
+        return "{}".format(self.username)
+
+#create the Song model here + add a nice representation method
+    
+#create the Item model here + add a nice representation method
+    
+#create the Playlist model here + add a nice representation method
